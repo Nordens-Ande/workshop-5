0b5a8f4710449e221c16939081f51e2ebe56a3c1
Elliot Nord <elliotnord@icloud.com>
2025-02-25 15:01:11 +0100

    3 Challenges gjorde på del 1
    dags för inlämning

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -6,20 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody characterRB;
-    //Transform characterTransform;
     Vector3 movementInput;
     Vector3 movementVector;
     [SerializeField] float movementSpeed;
     [SerializeField] float jumpForce;
 
-    private float sprint = 1;
+    private float speedMultiplier = 1;
     private bool grounded = true;
 
 
     void Start()
     {
         characterRB = GetComponent<Rigidbody>();    
-        //characterTransform = GetComponent<Transform>();
     }
 
     // Update is called once per frame
@@ -28,13 +26,17 @@ public class PlayerMovement : MonoBehaviour
         ApplyMovement();
     }
 
+
+    //Här apply-ar vi vår movement med hjälp utav den bestämda hastigheten, hastighetkonstanten (speedMultiplier, som bestäms av sprint och crouch) och rörelsevektorn
     private void ApplyMovement()
     {
         movementVector = transform.forward * movementInput.y + transform.right * movementInput.x;
         movementVector.y = 0;
-        transform.position += movementVector * sprint * movementSpeed * Time.deltaTime;
+        transform.position += movementVector * speedMultiplier * movementSpeed * Time.deltaTime;
     }
 
+
+    //Här genomför vi hoppet och kollar även ifall vi är på marken eller inte genom grounded
     private void OnJump(InputValue input)
     {
         if (!grounded) return;
@@ -42,26 +44,46 @@ public class PlayerMovement : MonoBehaviour
         characterRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
+
+    //Här ändrar vi hastighet konstanten för 'sprint'. OBS nu är det en överdriven förändring för att det ska vara lättare att se/märka
+    //Dessutom så ändrar vi objektets storlek för att visualisera att vi springer och för att det ser skitroligt ut
     private void OnSprint(InputValue input)
     {
-        sprint = 2;
+        speedMultiplier = 3;
+        transform.localScale = new Vector3(0.75f, 1, 1);
     }
-
     private void OnSprintStop(InputValue input)
     {
-        sprint = 1;
+        speedMultiplier = 1;
+        transform.localScale = new Vector3(1, 1, 1);
     }
+
+
+    //Här ändrar vi hastighet konstanten för 'crouch'. OBS nu är det en överdriven förändring för att det ska vara lättare att se/märka
+    //Dessutom så ändrar vi objektets storlek för att visualisera att vi crouch-ar
+    private void OnCrouch(InputValue input)
+    {
+        speedMultiplier = 0.25f;
+        transform.localScale = new Vector3(1, 0.5f, 1);
+    }
+    private void OnCrouchStop(InputValue input)
+    {
+        speedMultiplier = 1;
+        transform.localScale = new Vector3(1, 1, 1);
+    }
+
 
     private void OnMovement(InputValue input)
     {
         movementInput = input.Get<Vector2>();
     }
 
+
+    //Här är våran rigidbody kollisioner som ser till ifall vi kan hoppa eller inte med hjälp av grounded bool-en.
     private void OnCollisionEnter(Collision collision)
     {
         grounded = true;
     }
-
     private void OnCollisionExit(Collision collision)
     {
         grounded = false;
